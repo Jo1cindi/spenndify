34dbee0789b2d0b16cd93bbadb1cee8646193d83
Jo1cindi <joancindi535@gmail.com>
2022-07-27 14:23:52 +0300

    Building the expenses page plus responsiveness

--- src/Dashboard/Expenses.js
+++ src/Dashboard/Expenses.js
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../Components/Sidebar";
 import SidebarMobile from "../Components/SidebarMobile";
-import { FiSettings } from "react-icons/fi"
+import { FiSettings } from "react-icons/fi";
 
 const Expenses = () => {
+
+//Functions to toggle Month/Weeky buttons
+  const [active, setActive] = useState(true);
+  const Filter = () => {
+    setActive(!active)
+    setActiveMonthly(!activeMonthly)
+  };
+
+  const [activeMonthly, setActiveMonthly] = useState(false);
+  const filterMonthly = () => {
+    setActiveMonthly(!activeMonthly)
+    setActive(!active)
+    
+  };
+
+  //Getting the months
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  let monthIndex = (new Date().getMonth());
+  let monthName = months[monthIndex];
   return (
     <div className="Expenses">
       <Sidebar />
@@ -16,11 +49,43 @@ const Expenses = () => {
             <h3>Expense Report</h3>
           </div>
           <div className="settings">
-           <FiSettings className="settingsIcon"/>
+            <FiSettings className="settingsIcon" />
           </div>
         </div>
         <div className="filterExpenses">
-          
+          <div className="toggleBtn">
+            <div
+              className={active ? "filterbyWeekly" : "inactiveFilterbyWeekly"}
+              onClick={Filter}
+            >
+              <p>Weekly</p>
+            </div>
+
+            <div
+              className={
+                activeMonthly ? "filterbyMonthly" : "inactiveFilterbyMonthly"
+              }
+              onClick={filterMonthly}
+            >
+              <p>Monthly</p>
+            </div>
+          </div>
+        </div>
+        <div className="expenditurePieChart">
+          <p>Overview for {monthName}</p>
+          <div className="piechartCard">
+
+          </div>
+        </div>
+        <div className="expenditureBargraph">
+          <p>Expenditure overview</p>
+          <div className="bargraphCard">
+
+          </div>
+        </div>
+        <div className="categoriesPieChart">
+          <p>Spending Categories</p>
+          <div className="categoriesPiechartCard"></div>
         </div>
       </div>
     </div>
