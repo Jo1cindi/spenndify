84b8ca8db204a412e5b10e10600de8fe33f3a4d6
Jo1cindi <joancindi535@gmail.com>
2022-08-20 23:23:37 +0300

    changing verification url

--- src/Onboarding/EnterVerificationcode.js
+++ src/Onboarding/EnterVerificationcode.js
@@ -32,7 +32,7 @@ const EnterVerificationcode = () => {
     // e.preventdefault (); //Clearing input fields
 
     //Post Request
-    const url =  "https://cors-anywhere.herokuapp.com/https://spenndify-expenses-app.herokuapp.com/spendy/user/verify/registration/otp";
+    const url =  "https://spenndify-expenses-app.herokuapp.com/spendy/user/verify/registration/otp";
     const otpCode = {
       "receivedOtp": verificationCode,
       "phone": phoneNumber
